834ccede16c92549349a2b86cbf58ef8cb6dfe4a
Rodrigo Gil <rgilpt@gmail.com>
2022-03-27 21:56:36 +0100

    +Add: Level X.3

--- Level X.2/main.py
+++ Level X.2/main.py
@@ -1,68 +1,132 @@
+import pygame
 import random
-import matplotlib.pyplot as plt
+import math
 
-list_numbers = []
-list_unique_numbers = []
-list_count_numbers = []
+# initiate pygame
+pygame.init()
 
+# set screen res
+screen = pygame.display.set_mode((800, 600))
 
-def create_random_numbers():
-    for i in range(0, 1000):
-        new_number = random.randrange(0, 100)
-        list_numbers.append(new_number)
+# background
+background = pygame.image.load("spaceBackground.jpg")
 
+# title and icon
+pygame.display.set_caption("I'll change this later")
+icon = pygame.image.load("Eyeball - for project.png")
+pygame.display.set_icon(icon)
 
-def create_unique_list():
-    for n in list_numbers:
-        if n not in list_unique_numbers:
-            list_unique_numbers.append(n)
+# player
+playerImg = pygame.image.load("fireModDude(project).png")
+playerX = 400
+playerY = 538
+playerXChange = 0
 
+#lives
+lives = 3
+#score (when asteroid dodged)
+score = 0
 
-def create_count_numbers():
-    for n in list_unique_numbers:
-        list_count_numbers.append(list_numbers.count(n))
+# asteroid
+asteroidImg = []
+asteroidX = []
+asteroidY = []
+asteroidStartingX = []
+asteroidAngle = []
+numOfAsteroids = 6
 
-def print_numbers_count():
-    c = 0
-    for i in list_unique_numbers:
-        print("{} -> {}".format(i, list_count_numbers[c]))
-        c += 1
-
-
-def average(list_count):
-
-    average_count = 0
-
-    for c in list_count:
-        average_count += c
-
-    return (average_count * 1.0)/(len(list_count)*1.0)
-
-
-def analysis(list_count):
-    x = []
-    y = []
-    for c in range(1, 20):
-        count = list_count.count(c)
-        print(count)
-        x.append(c)
-        y.append(count)
-
-    plt.plot(x, y)
-    plt.show()
+for i in range(numOfAsteroids):
+    asteroidImg.append(pygame.image.load("asteroid.png"))
+    x = random.randint(0, 768)
+    asteroidX.append(x)
+    asteroidStartingX.append(x)
+    asteroidY.append(-32)
+    asteroidAngle.append(random.uniform(0.15, 0.9))
 
 
-def main():
-
-    create_random_numbers()
-    create_unique_list()
-    # list_unique_numbers.sort()
-    create_count_numbers()
-    print_numbers_count()
-    print(average(list_count_numbers))
-    analysis(list_count_numbers)
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 
-if __name__ == '__main__':
-    main()
+def asteroid(x, y, i):
+    screen.blit(asteroidImg[i], (x, y))
 
+
+def isCollision(asteroidX, asteroidY, playerX, playerY):
+    distance = math.sqrt((math.pow(asteroidX - playerX, 2)) + (math.pow(asteroidY - playerY, 2)))
+    if distance < 27:
+        return True
+    else:
+        return False
+
+
+# game loop
+running = True
+while running:
+
+    # RGB background
+    screen.fill((57, 70, 78))
+
+    # background img
+    screen.blit(background, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # movement
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerXChange = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerXChange = 0.3
+            if event.key != pygame.K_LEFT and event.key != pygame.K_LEFT:
+
+                # to make it stop when key released
+                # if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                #     playerXChange = 0
+
+                # moves when no input is detected
+                if event.key == pygame.K_LEFT:
+                    playerXChange = -0.3
+                if event.key == pygame.K_RIGHT:
+                    playerXChange = 0.3
+
+    playerX += playerXChange
+    asteroidY += asteroidAngle
+
+    # makes asteroids move diagonally to some extent rather than straight
+    if asteroidStartingX[i] <= 384:
+        asteroidX[i] += random.uniform(0.05, 0.25)
+    elif asteroidStartingX[i] >= 384:
+        asteroidX[i] -= random.uniform(0.05, 0.25)
+
+    # respawns asteroid
+    for i in range(numOfAsteroids):
+        if asteroidY[i] >= 600:
+            score += 1
+            asteroidX[i] = random.randint(0, 768)
+            asteroidStartingX[i] = asteroidX[i]
+            asteroidY[i] = -32
+            asteroidAngle[i] = random.uniform(0.3, 1)
+
+        # collision
+        collision = isCollision(asteroidX[i], asteroidY[i], playerX, playerY)
+        if collision:
+            asteroidX[i] = random.randint(0, 768)
+            asteroidStartingX[i] = asteroidX[i]
+            asteroidY[i] = -32
+            asteroidAngle[i] = random.uniform(0.3, 1)
+            lives -= 1
+            print(lives)
+
+        asteroid(asteroidX[i], asteroidY[i], i)
+
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 768:
+        playerX = 768
+
+    player(playerX, playerY)
+    pygame.display.update()
---
+++ Level X.3/main.py
@@ -0,0 +1,35 @@
+
+
+city = ["Solboa", "Raris", "Lonpon", "Bodrid", "Serlin"]
+
+
+temperatures = [
+    {
+        "city": "Solboa",
+        "temperatures": [13, 14, 14, 12, 11, 15, 16, 15, 15, 14, 13, 12, 9]
+    },
+    {
+        "city": "Raris",
+        "temperatures": [10, 11, 12, 11, 23, 9, 7, 7, 8, 10, 11, 11, 8]
+    },
+    {
+        "city": "Lonpon",
+        "temperatures": [8, 8, 7, 8, 4, 5, 3, 3, 4, 7, 8, 9, 8]
+    },
+    {
+        "city": "Bodrid",
+        "temperatures": [12, 14, 114, 15, 14, 11, 10, 10, 1, 9, 9, 10, 8]
+    },
+    {
+        "city": "Serlin",
+        "temperatures": [283, 284, 285, 284, 286, 286, 283, 282, 281, 281, 280, 281, 282]
+    },
+
+]
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
