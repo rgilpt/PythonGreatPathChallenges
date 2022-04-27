48f82d6c81effa33a14f42841e9f55ed3a2998b2
Rodrigo Gil <rgilpt@gmail.com>
2022-04-27 07:43:42 +0100

    Add: Level X.4

---
+++ Level X.4/main.py
@@ -0,0 +1,36 @@
+import json
+
+
+def load_data_doctors(filename="doctors.json"):
+    data = None
+    with open(filename) as f:
+        data = json.load(f)
+
+    return data
+
+
+def load_data_patients(filename="patients.json"):
+    data = None
+    with open(filename) as f:
+        data = json.load(f)
+
+    return data
+
+
+def load_data():
+
+    data = {
+        'doctors': load_data_doctors(),
+        'patients': load_data_patients()
+    }
+    return data
+
+
+def main():
+    my_data = load_data()
+    print(my_data)
+    print("Done!")
+
+
+if __name__ == '__main__':
+    main()
